4b7f432ebe618ff2abc5d08ec1573aadb36799f4
Antonio <tonyantoniopaulo@gmail.com>
2024-04-01 20:56:43 -0300

    few details changed on sistema models.py

--- sistema/models.py
+++ sistema/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.db import models
 
 # Create your models here.
 
@@ -8,9 +10,9 @@ from django.apps import AppConfig
 class game(models.Model):
     name = models.CharField(max_length=50) #A API tem q mandar pra esse models ok?
     #plataformas???
-    wishlisted = models.BooleanField(default=False) # ou é true ou é falso
 
 class usuario(models.Model):
     userName = models.CharField(max_length=50)
     dataDeGamesDoUser = game # cada user vai ter seu dado de games(?)
+    #wishlist array
 
