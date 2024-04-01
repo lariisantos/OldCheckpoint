edf0dc9b4c04f319fc11b0fe7e03ab0bf12ab2a7
Antonio <tonyantoniopaulo@gmail.com>
2024-04-01 20:42:39 -0300

    Made models and added sistema apps

--- sistema/models.py
+++ sistema/models.py
@@ -8,7 +8,7 @@ from django.apps import AppConfig
 class game(models.Model):
     name = models.CharField(max_length=50) #A API tem q mandar pra esse models ok?
     #plataformas???
-    wishlisted = models.BooleanField() # ou é true ou é falso
+    wishlisted = models.BooleanField(default=False) # ou é true ou é falso
 
 class usuario(models.Model):
     userName = models.CharField(max_length=50)
