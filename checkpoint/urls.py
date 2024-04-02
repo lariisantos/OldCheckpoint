33ea73dc04121da77b1bc17d4df54d03e6c21fe3
JotaPeans <jpfontesferreira1@gmail.com>
2024-04-01 21:58:53 -0300

    fix: requirements, urls,

--- checkpoint/urls.py
+++ checkpoint/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sistema.urls'))
+    path('', include('autenticacao.urls')),
+    path('app/', include('sistema.urls'))
 ]
--- sistema/models.py
+++ sistema/models.py
@@ -1,18 +1,16 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
-from django.apps import AppConfig
-
-
-class game(models.Model):
+class Game(models.Model):
     name = models.CharField(max_length=50) #A API tem q mandar pra esse models ok?
-    #plataformas???
+    imageUrl = models.CharField(max_length=200, default=False)
+    platforms = models.JSONField(default=dict)
+    likes = models.IntegerField(default=0)
 
-class usuario(models.Model):
-    userName = models.CharField(max_length=50)
-    dataDeGamesDoUser = game # cada user vai ter seu dado de games(?)
-    #wishlist array
-
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário')
+    wishList = models.ManyToManyField(Game, related_name='wishlist')
+    favorites = models.ManyToManyField(Game, related_name='favorites')
